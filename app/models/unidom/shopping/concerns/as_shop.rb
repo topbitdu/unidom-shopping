b213d9bb9185ce1c008ede03867bd7d5da2340e7
Topbit Du <topbit.du@gmail.com>
2016-10-10 09:55:39 +0800

    1, Improve the As Shop concern to add the #get_cart! method.

--- app/models/unidom/shopping/concerns/as_shop.rb
+++ app/models/unidom/shopping/concerns/as_shop.rb
@@ -6,6 +6,10 @@ module Unidom::Shopping::Concerns::AsShop
 
     has_many :shopping_carts, class_name: 'Unidom::Shopping::ShoppingCart', as: :shop
 
+    def get_cart!(by: nil, at: Time.now)
+      shopping_carts.shopped_by(by).valid_at(now: at).alive.first_or_create! opened_at: at
+    end
+
   end
 
   module ClassMethods
