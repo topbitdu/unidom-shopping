380f2547d289f4494bffd2f915bda902abd45263
Topbit Du <topbit.du@gmail.com>
2017-01-27 14:41:16 +0800

    1, Models RSpec examples manifest.

---
+++ lib/unidom/shopping/models_rspec.rb
@@ -0,0 +1,2 @@
+require 'rspec/models/unidom/shopping/shopping_cart_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Shopping::ShoppingCart'
+require 'rspec/models/unidom/shopping/shopping_item_spec' unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Shopping::ShoppingItem'
