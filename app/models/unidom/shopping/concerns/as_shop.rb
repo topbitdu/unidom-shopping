37c411dc19a730736131ec65d9458a9c8cac1dfb
Topbit Du <topbit.du@gmail.com>
2017-02-12 09:51:24 +0800

    1, Improve the As Shop concern for the document.

--- app/models/unidom/shopping/concerns/as_shop.rb
+++ app/models/unidom/shopping/concerns/as_shop.rb
@@ -9,6 +9,9 @@ module Unidom::Shopping::Concerns::AsShop
 
     has_many :shopping_carts, class_name: 'Unidom::Shopping::ShoppingCart', as: :shop
 
+    ##
+    # 从当前商店中，获取购物车。购物者为 by 。获取时间为 at ，缺省为当前时间。如：
+    # shop.get_cart! by: current_person
     def get_cart!(by: nil, at: Time.now)
       shopping_carts.shopped_by(by).valid_at(now: at).alive.first_or_create! opened_at: at
     end
