d3f25e7e1ed6a214253e50492f4ea9330d1e4f3b
Topbit Du <topbit.du@gmail.com>
2017-02-27 18:15:55 +0800

    1, Improve the Shopping Cart spec for the ``has_many :items, class_name: 'Unidom::Shopping::ShoppingItem'`` macro.

--- lib/rspec/models/unidom/shopping/shopping_cart_spec.rb
+++ lib/rspec/models/unidom/shopping/shopping_cart_spec.rb
@@ -17,6 +17,26 @@ describe Unidom::Shopping::ShoppingCart, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
+    shopping_item_1_attributes = {
+      shopper_id:       SecureRandom.uuid,
+      shopper_type:     'Unidom::Shopping::Shopper',
+      shopped_id:       SecureRandom.uuid,
+      shopped_type:     'Unidom::Shopping::Shopped',
+      unit_price:       12.00,
+      quantity:         5.00
+    }
+
+    shopping_item_2_attributes = {
+      shopper_id:       SecureRandom.uuid,
+      shopper_type:     'Unidom::Shopping::Shopper',
+      shopped_id:       SecureRandom.uuid,
+      shopped_type:     'Unidom::Shopping::Shopped',
+      unit_price:       19.98,
+      quantity:         2.00
+    }
+
+    it_behaves_like 'has_many', model_attributes, :items, Unidom::Shopping::ShoppingItem, [ shopping_item_1_attributes, shopping_item_2_attributes ]
+
   end
 
 end
