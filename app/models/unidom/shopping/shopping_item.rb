0ed29ddc50c650c17c58edfd825070cbf9f99a0e
Topbit Du <topbit.du@gmail.com>
2017-03-13 09:20:18 +0800

    1, Improve the Shopping Item model for the validations on the #unit_price attribute, & the #quantity attribute.

--- app/models/unidom/shopping/shopping_item.rb
+++ app/models/unidom/shopping/shopping_item.rb
@@ -7,15 +7,15 @@ class Unidom::Shopping::ShoppingItem < Unidom::Shopping::ApplicationRecord
 
   include Unidom::Common::Concerns::ModelExtension
 
-  validates :unit_price, presence: true, numericality: { greater_than: 0, less_than: 1_000_000_000 }
-  validates :quantity,   presence: true, numericality: { greater_than: 0, less_than: 1_000_000_000 }
+  validates :unit_price, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1_000_000_000 }
+  validates :quantity,   presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1_000_000_000 }
 
   belongs_to :shopping_cart, class_name: 'Unidom::Shopping::ShoppingCart'
   belongs_to :shopper,       polymorphic: true
   belongs_to :shopped,       polymorphic: true
 
-  scope :shopping_cart_is, ->(shopping_cart) { where shopping_cart_id: (shopping_cart.respond_to?(:id) ? shopping_cart.id : shopping_cart) }
-  scope :shopped_by,       ->(shopper)       { where shopper: shopper }
-  scope :shopped_is,       ->(shopped)       { where shopped: shopped }
+  scope :shopping_cart_is, ->(shopping_cart) { where shopping_cart_id: to_id(shopping_cart) }
+  scope :shopped_by,       ->(shopper)       { where shopper:          shopper              }
+  scope :shopped_is,       ->(shopped)       { where shopped:          shopped              }
 
 end unless Unidom::Common::Neglection.namespace_neglected? 'Unidom::Shopping::ShoppingItem'
