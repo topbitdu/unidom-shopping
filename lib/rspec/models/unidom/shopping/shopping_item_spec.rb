9788808acfd193f73efff4d4d6a0d80020a32eb8
Topbit Du <topbit.du@gmail.com>
2017-03-14 10:26:01 +0800

    1, Improve the Shopping Item spec for the validations on the #unit_price attribute, & the #quantity attribute.

--- lib/rspec/models/unidom/shopping/shopping_item_spec.rb
+++ lib/rspec/models/unidom/shopping/shopping_item_spec.rb
@@ -20,49 +20,10 @@ describe Unidom::Shopping::ShoppingItem, type: :model do
 
     it_behaves_like 'Unidom::Common::Concerns::ModelExtension', model_attributes
 
-    it_behaves_like 'validates', model_attributes, :unit_price,
-      {                                } => 0,
-      { unit_price: nil                } => 2,
-      { unit_price: ''                 } => 2,
-      { unit_price: '1'                } => 0,
-      { unit_price: 1                  } => 0,
-      { unit_price: 'A'                } => 1,
-      { unit_price: '1.23'             } => 0,
-      { unit_price: 1.23               } => 0,
-      { unit_price: '-0.01'            } => 1,
-      { unit_price: -0.01              } => 1,
-      { unit_price: '0'                } => 1,
-      { unit_price: 0                  } => 1,
-      { unit_price: '0.01'             } => 0,
-      { unit_price: 0.01               } => 0,
-      { unit_price: '999_999_999.99'   } => 0,
-      { unit_price: 999_999_999.99     } => 0,
-      { unit_price: '1_000_000_000'    } => 1,
-      { unit_price: 1_000_000_000      } => 1,
-      { unit_price: '1_000_000_000.01' } => 1,
-      { unit_price: 1_000_000_000.01   } => 1
-
-    it_behaves_like 'validates', model_attributes, :quantity,
-      {                              } => 0,
-      { quantity: nil                } => 2,
-      { quantity: ''                 } => 2,
-      { quantity: '1'                } => 0,
-      { quantity: 1                  } => 0,
-      { quantity: 'A'                } => 1,
-      { quantity: '1.23'             } => 0,
-      { quantity: 1.23               } => 0,
-      { quantity: '-0.01'            } => 1,
-      { quantity: -0.01              } => 1,
-      { quantity: '0'                } => 1,
-      { quantity: 0                  } => 1,
-      { quantity: '0.01'             } => 0,
-      { quantity: 0.01               } => 0,
-      { quantity: '999_999_999.99'   } => 0,
-      { quantity: 999_999_999.99     } => 0,
-      { quantity: '1_000_000_000'    } => 1,
-      { quantity: 1_000_000_000      } => 1,
-      { quantity: '1_000_000_000.01' } => 1,
-      { quantity: 1_000_000_000.01   } => 1
+    it_behaves_like 'validates numericality', model_attributes, :unit_price,
+      range: 0..1_000_000_000, minimum_inclusive: true, maximum_inclusive: true
+    it_behaves_like 'validates numericality', model_attributes, :quantity,
+      range: 0..1_000_000_000, minimum_inclusive: true, maximum_inclusive: true
 
     shopping_cart_attributes = {
       shopper_id:   SecureRandom.uuid,
