c19914a6912da1729f2330a4b1f8db12674838cc
Topbit Du <topbit.du@gmail.com>
2017-04-12 10:11:28 +0800

    1, Improve the RSpec shared examples manifest to require the As Cart Shopper shared examples.

--- lib/unidom/shopping/rspec_shared_examples.rb
+++ lib/unidom/shopping/rspec_shared_examples.rb
@@ -1,2 +1,3 @@
+require 'rspec/models/unidom/shopping/concerns/as_cart_shopper_shared_examples'
 require 'rspec/models/unidom/shopping/concerns/as_shop_shared_examples'
 require 'rspec/models/unidom/shopping/concerns/as_shopped_shared_examples'
